2e20852153d3518ecf9a1930ebd55822756ff3ee
Daniel Rapp <danielrappt@gmail.com>
2015-02-20 14:49:45 +0100

    Added checklist and basic edit-page

--- routes/index.js
+++ routes/index.js
@@ -36,6 +36,7 @@ exports = module.exports = function(app) {
 	
 	// Views
 	app.get('/', routes.views.index);
+	app.get('/create', routes.views.create);
 	
 	
 	// NOTE: To protect a route so that only admins can see it, use the requireUser middleware:
--- routes/views/index.js
+++ routes/views/index.js
@@ -8,8 +8,19 @@ exports = module.exports = function(req, res) {
   // item in the header navigation.
   locals.section = 'home';
   locals.checks = [
-    { articleName: "Tandborste", hylla: "42" },
-    { articleName: "Knivbland", hylla: "43" }
+    { articleName: "Abs Cilikonförband 5X12,5Cm Självhäftande",     storage: "NS", section: "39", shelf: "F", tray: "1" },
+    { articleName: "Abs sökongörband 5 x 12,5Cm självhäftande",     storage: "SS", section: "32", shelf: "D", tray: "2" },
+    { articleName: "Abs. Material , Strässel (T. Kemikalier)",      storage: "NO", section: "B", shelf: "B4", tray: "2" },
+    { articleName: "Abs.Skydd Föxerinfbyxa",                        storage: "NO", section: "B", shelf: "B3", tray: "11" },
+    { articleName: "Abs.Skydd Får Herrar Räbot",                    storage: "NO", section: "B", shelf: "B2", tray: "4" },
+    { articleName: "Abs-fårband 10x10 cm",                          storage: "SS", section: "33", shelf: "F", tray: "2" },
+    { articleName: "Abs-Forband 12X10Cm (S)",                       storage: "NS", section: "38", shelf: "A", tray: "1" },
+    { articleName: "Abs-förband 11x20cm",                           storage: "SS", section: "33", shelf: "F", tray: "1" },
+    { articleName: "Abs-förband 24x40",                             storage: "SS", section: "33", shelf: "G", tray: "1" },
+    { articleName: "Abs-Förband 28X40Cm (S)",                       storage: "NS", section: "38", shelf: "A", tray: "2" },
+    { articleName: "Abs-Förband Haft 10X35Cm (S)",                  storage: "NS", section: "38", shelf: "B", tray: "4" },
+    { articleName: "Absorber Co3",                                  storage: "NO", section: "42", shelf: "F+G", tray: "" },
+    { articleName: "Absorbtionshalk, Infinity ID CLICK absorbtion", storage: "SO", section: "45", shelf: "D", tray: "" },
   ];
 
   // Render the view
