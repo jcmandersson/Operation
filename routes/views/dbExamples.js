07d88524dde17a302a16740b1cb16178997bff25
Jonas Andersson <jcm.andersson@gmail.com>
2015-02-21 14:02:53 +0100

    Added page for database examples

---
+++ routes/views/dbExamples.js
@@ -0,0 +1,18 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+  var view = new keystone.View(req, res),
+      locals = res.locals;
+
+  // locals.section is used to set the currently selected
+  // item in the header navigation.
+  locals.section = 'dbExamples';
+  
+  locals.db = {
+    
+  };
+  
+  // Render the view
+  view.render('dbExamples');
+
+};
