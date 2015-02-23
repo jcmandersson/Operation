fbde304a47850c1d2e9bfc8a419e8f34d1c27059
Daniel Falk <fannedalk@gmail.com>
2015-02-23 10:59:31 +0100

    search Kartotekarticles

--- public/js/create.js
+++ public/js/create.js
@@ -10,6 +10,39 @@ $( "#article" ).change(function() {
   
 });
 
+$.ajax({
+  type: 'GET',
+  url: '/api/Kartotek'+' '+'artikels',
+  data: { //Lämna tom för att få ut all data
+    tags: 'knife'
+  }
+})
+  .done(function( msg ) {
+    console.log(msg);
+  })
+  .fail(function(err, status){
+    console.log('Någonting gick fel!');
+    console.log(err);
+    console.log(status);
+  });
+
+$.ajax({
+  type: 'GET',
+  url: '/api/operations',
+  data: { //Lämna tom för att få ut all data
+    title: 'Blind'
+  }
+})
+  .done(function( msg ) {
+    console.log("hej");
+    console.log(msg);
+  })
+  .fail(function(err, status){
+    console.log('Någonting gick fel!');
+    console.log(err);
+    console.log(status);
+  });
 
 
+//console.log(Operation.models.KartotekArticle.schema.statics.search("Kniv"));
 
--- routes/views/create.js
+++ routes/views/create.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+var kartotekArticle = keystone.list('Kartotek artikel');
 
 exports = module.exports = function(req, res) {
   var view = new keystone.View(req, res),
@@ -8,7 +9,20 @@ exports = module.exports = function(req, res) {
   // item in the header navigation.
   locals.section = 'create';
   
+  kartotekArticle.model.search('knife', function (err, data) {
+    if (err) {
+      console.log('DB error');
+      return;
+    }
+    console.log(data);
+    locals.db = data[0].name;
+    //locals.db = data.toString();
+    view.render('create');
+  });
+  
+  
+  
   // Render the view
-  view.render('create');
+  //view.render('create');
 
 };
