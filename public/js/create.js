60da73fdafb0385b9c126ccdecaec23acb552776
Daniel Falk <fannedalk@gmail.com>
2015-02-23 12:00:14 +0100

    can remove articles

--- public/js/create.js
+++ public/js/create.js
@@ -1,48 +1,29 @@
-$('.remove').click(function(){
-  $('.item').hide();
+$('.articles').on('click', '.remove', function(){
+  $(this).parent().remove();
 });
-
 
 $( "#article" ).change(function() {
   var bla = $('#article').val();
   console.log(bla);
-  $( ".articles" ).append( "<div class='item'> <span class='glyphicon glyphicon-remove-circle remove' aria-hidden='true'></span> <span class='item-text'>"+bla+"</span> </div>" );
+
+  $.ajax({
+    type: 'GET',
+    url: '/api/Kartotekartikels/'+bla
+  })
+    .done(function( msg ) {
+      
+      console.log(msg);
+      $( ".articles" ).append( "<div class='item' id="+msg._id+"> <span class='glyphicon glyphicon-remove-circle remove' aria-hidden='true'></span> <span class='item-text'>"+msg.name+"</span> </div>" );
+
+    })
+    .fail(function(err, status){
+      console.log('Någonting gick fel!');
+      console.log(err);
+      console.log(status);
+    });
+  
+  
   
 });
 
-$.ajax({
-  type: 'GET',
-  url: '/api/Kartotek'+' '+'artikels',
-  data: { //Lämna tom för att få ut all data
-    tags: 'knife'
-  }
-})
-  .done(function( msg ) {
-    console.log(msg);
-  })
-  .fail(function(err, status){
-    console.log('Någonting gick fel!');
-    console.log(err);
-    console.log(status);
-  });
-
-$.ajax({
-  type: 'GET',
-  url: '/api/operations',
-  data: { //Lämna tom för att få ut all data
-    title: 'Blind'
-  }
-})
-  .done(function( msg ) {
-    console.log("hej");
-    console.log(msg);
-  })
-  .fail(function(err, status){
-    console.log('Någonting gick fel!');
-    console.log(err);
-    console.log(status);
-  });
-
-
-//console.log(Operation.models.KartotekArticle.schema.statics.search("Kniv"));
 
--- routes/views/create.js
+++ routes/views/create.js
@@ -1,5 +1,4 @@
 var keystone = require('keystone');
-var kartotekArticle = keystone.list('Kartotek artikel');
 
 exports = module.exports = function(req, res) {
   var view = new keystone.View(req, res),
@@ -9,20 +8,7 @@ exports = module.exports = function(req, res) {
   // item in the header navigation.
   locals.section = 'create';
   
-  kartotekArticle.model.search('knife', function (err, data) {
-    if (err) {
-      console.log('DB error');
-      return;
-    }
-    console.log(data);
-    locals.db = data[0].name;
-    //locals.db = data.toString();
-    view.render('create');
-  });
-  
-  
-  
   // Render the view
-  //view.render('create');
+  view.render('create');
 
 };
