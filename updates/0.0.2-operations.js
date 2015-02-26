d2e5d8acfee5d40f58ccfecf9c0f4b8eb53b5cea
Albert Karlsson <albka735@student.liu.se>
2015-02-26 08:38:42 +0100

    Added some users

---
+++ updates/0.0.2-operations.js
@@ -0,0 +1,35 @@
+/**
+ * Created by abbe on 2015-02-23.
+ */
+
+exports.create = {  
+	User: [
+		{ 'name.first': 'Daniel', 'name.last': 'Rapp', email: 'danth407@student.liu.se', password: 'daniel', isAdmin: true },
+    { 'name.first': 'Jonas', 'name.last': 'Andersson', email: 'jonan111@student.liu.se', password: 'jonas', isAdmin: true },
+    { 'name.first': 'Albert', 'name.last': 'Karlsson', email: 'albka735@student.liu.se', password: 'albert', isAdmin: true },
+    { 'name.first': 'Daniel', 'name.last': 'Falk', email: 'palka285@student.liu.se', password: 'daniel', isAdmin: true },
+    { 'name.first': 'Pål', 'name.last': 'Kastman', email: 'jonas@halvbra.se', password: 'pal', isAdmin: true },
+    { 'name.first': 'Erik', 'name.last': 'Malmberg', email: 'erima694@student.liu.se', password: 'erik', isAdmin: true },
+    { 'name.first': 'Robin', 'name.last': 'Andersson', email: 'roban563@student.liu.se', password: 'robin', isAdmin: true },
+    { 'name.first': 'Not', 'name.last': 'Admin', email: 'asdas@afs.se', password: 'user', isAdmin: false }
+	]/*,
+  Operations: [
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true},
+    { title: 'ACI Armbåge', tags: '', state: 'Publicerad', speciality: '', template: true}
+  ]*/
+};
