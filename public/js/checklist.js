a120a5f5fd904e1c67f51bf7f87ae5f472fac659
Robin Andersson <robin.andersson93@hotmail.com>
2015-03-04 16:35:46 +0100

    Made checklists uncheckable if the operation is a template.

--- public/js/checklist.js
+++ public/js/checklist.js
@@ -6,7 +6,9 @@ var operationId;
 
 $(function(){
   $('.check-js').click(function() {
-    socket.emit('checkboxClick', {operationId:operationId, id:$(this).attr('id')});
+    if(!$(this).prop('disabled')) {
+      socket.emit('checkboxClick', {operationId: operationId, id: $(this).attr('id')});
+    }
   });
 });
 
@@ -15,23 +17,32 @@ socket.on('connect', function(){ //Runs after socket has been started.
   socket.emit('operationOpen', operationId);
 });
 
-var updateTableRow = function(tableRow, isChecked){
+var updateTableRow = function(tableRow, isChecked, isTemplate){
   var checkbox = tableRow.find('input');
   checkbox.prop('checked', isChecked);
+  console.log("i update: " + isTemplate);
+  if(isTemplate){
+    checkbox.prop('disabled', true);
+    tableRow.prop('disabled', true);
+  }
+  
   changeTableGraphics(tableRow, isChecked); //Function in checkEffect.js
 };
 
 socket.on('checkboxClick', function(checkObject){
   var tableRow = $('#' + checkObject.id);
-  updateTableRow(tableRow, checkObject.isChecked);
+  updateTableRow(tableRow, checkObject.isChecked, false);
 });
 
-socket.on('getCheckboxes', function(checkboxes){
+socket.on('getCheckboxes', function(checkboxesAndTemplate){
+  var checkboxes = checkboxesAndTemplate.checkboxes;
+  var isTemplate = checkboxesAndTemplate.template;
+  console.log("i get: " + isTemplate);
   for(var index in checkboxes){
     var checkbox = checkboxes[index];
     var tableRow = $('#' + checkbox._id);
     var isChecked = checkbox.checked;
     //var isDisabled = checkbox.attr("disabled");
-    updateTableRow(tableRow, isChecked);
+    updateTableRow(tableRow, isChecked, isTemplate);
   }
 });
