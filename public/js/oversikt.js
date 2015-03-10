46e176f9c4a5de13572d43d36a29c0ebbfadcaaa
Jonas Andersson <jcm.andersson@gmail.com>
2015-03-10 20:03:20 +0100

    Updating frontpage with socket.io

--- public/js/oversikt.js
+++ public/js/oversikt.js
@@ -1,3 +1,9 @@
+function calculateProgress(d){
+  if(!d.total || !d.checked) return 1;
+  console.log(d.checked/ d.total);
+  return 100*d.checked/ d.total;
+}
+
 $(document).ready(function(){
   
   $('.progressbar').each(function(i, e){
@@ -10,6 +16,16 @@ $(document).ready(function(){
       //.html('&nbsp;&nbsp;&nbsp;' + $(e).attr('data-checked') + ' / ' + $(e).attr('data-total'))
       .html('&nbsp;&nbsp;&nbsp;' + percentage + '% (' + $(e).attr('data-checked') + '/' + $(e).attr('data-total') + ')')
       .css("display", "block");
+  });
+
+  var socket = io();
+  
+  socket.on('updateProgress', function (progress) {
+    console.log(progress);
+    var percent = calculateProgress(progress.all);
+    var $progress = $('[data-id="'+progress.operation+'"] .progressbar').progressbar("value", percent);
+    if(percent === 1) percent--;
+    $progress.find('.ui-progressbar-value').html('&nbsp;&nbsp;&nbsp;'+percent+'% ('+progress.all.checked+'/'+progress.all.total+')');
   });
   
 });
