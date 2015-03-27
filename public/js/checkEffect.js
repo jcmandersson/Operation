6822ba32291ddc6c805df43ace44cab441709e78
Robin Andersson <robin.andersson93@hotmail.com>
2015-03-27 13:40:18 +0100

    Working color change on comment buttons on plocklistor.

--- public/js/checkEffect.js
+++ public/js/checkEffect.js
@@ -6,3 +6,13 @@ var changeTableGraphics = function(tablerow, isChecked){//Changes the color of t
     tablerow.css('background-color', 'rgba(0,0,0,0)');
   }
 };
+
+var changeCommentButton = function (comment, commentButton) {
+  if(comment.val() == "-") comment.val("");
+  if(comment.val() != ""){
+    commentButton.css('background-color','red');
+  }
+  else{
+    commentButton.css('background-color', 'blue');
+  }
+};
