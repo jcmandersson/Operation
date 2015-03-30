d6b3e2534a70c8e8f10f871688f1c62cbc60b5a6
Robin Andersson <robin.andersson93@hotmail.com>
2015-03-30 16:35:38 +0200

    Refactored some code and commented.

--- public/js/checklist.js
+++ public/js/checklist.js
@@ -7,19 +7,21 @@ var oldText;
 var saved = false;
 
 $(function(){
+  
   var rows = $('.check-js');
-  rows.each(function(index, row){
+  rows.each(function(index, row){ //Set the color of the comment button depending on if there is a comment.
     var comment = $('#checkComment' + row.id);
     var commentButton = $('#commentButton' + row.id);
     changeCommentButton(comment, commentButton);
   });
 
-  rows.click(function(e) {
+  rows.click(function(e) {  //When a checkable row is clicked, check the row and emit to socket.io
     if(!(e.target.tagName == 'P' || e.target.tagName == 'BUTTON')) {
       if (!$(this).prop('disabled')) {
         var checkbox = $(this).find('input')[0];
-        checkbox.checked = !checkbox.checked;
         var preparation = $(this).hasClass("process-content-item") ? true : false;
+        
+        checkbox.checked = !checkbox.checked;
         changeTableGraphics($(this), checkbox.checked, preparation); //Function in checkEffect.js
         var checkObject = {preparation: $(this).data('preparation'), operation: operationId, id: $(this).attr('id'), check: checkbox.checked};
         socket.emit('checkboxClick', checkObject);
@@ -27,11 +29,12 @@ $(function(){
     }
   });
 
-  $('.checkbox-js').click(function() {
+  //If the actual checkbox is clicked we will get a double-click effect. So we compensate for that here.
+  $('.checkbox-js').click(function() {  
     this.checked = !this.checked;
   });
 
-  $('.cancelComment').click(function(){
+  $('.cancelComment').click(function(){ //Throw away texted comment if cancel button is clicked.
     if(!saved) {
       var id = $(this).data('id');
       var checkComment = $('#checkComment' + id);
@@ -41,41 +44,45 @@ $(function(){
     }
   });
 
-  $('.saveComment').click(function(){
+  $('.saveComment').click(function(){ //Save the comment locally and emit to back-end to save in database.
     var id = $(this).data('id');
     var checkComment = $('#checkComment' + id);
+    var commentButton = $('#commentButton' + id);
+    
     if (checkComment.val() == ""){
       checkComment.val("-");
     }
+    
     var commentObject = {operation: operationId, id: id, comment: checkComment.val()};
     socket.emit('saveComment', commentObject);
-    var commentButton = $('#commentButton' + id);
     changeCommentButton(checkComment, commentButton);
+    
     checkComment.attr('disabled', true);
     $(this).attr('disabled', true);
     saved = true;
   });
 
-  $('.showComment').click(function(){
+  $('.showComment').click(function(){ //Hide the "Saved" text, enable the comment field and save the old text if the user presses cancel.
     var id = $(this).data('id');
-    $('#commentSaved' + id).hide();
     var checkComment = $("#checkComment" + id);
+    
+    $('#commentSaved' + id).hide();
     checkComment.attr('disabled', false);
     oldText = checkComment.val();
     $('#saveComment' + id).attr('disabled', false);
   });
 });
 
-socket.on('saved', function(id){
+socket.on('saved', function(id){ //Show saved text when the database has successfully stored the comment.
   $('#commentSaved' + id).show();
 });
 
-socket.on('connect', function(){ //Runs after socket has been started.
+socket.on('connect', function(){ //Runs after socket has been started. Get all checkbox statuses from db.
   operationId = $('.check-js').attr('data-operationId');
   socket.emit('operationOpen', operationId);
 });
 
-var updateTableRow = function(tableRow, isChecked, isTemplate){
+var updateTableRow = function(tableRow, isChecked, isTemplate){ //set checked status and update row color.
   var checkbox = tableRow.find('input');
   checkbox.prop('checked', isChecked);
   var preparation = tableRow.hasClass("process-content-item") ? true : false;
