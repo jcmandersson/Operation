ef74cf98ce714c3ca0a6bd03acfaf919157a0686
Robin Andersson <robin.andersson93@hotmail.com>
2015-03-31 10:56:25 +0200

    Fixed a bug in pdf-export.

--- routes/views/apiPrint.js
+++ routes/views/apiPrint.js
@@ -3,11 +3,13 @@
  */
 var keystone = require('keystone');
 var wkhtmltopdf = require('wkhtmltopdf');
+var os = require('os');
 
 exports = module.exports = function (req, res) {
-  if (os.platform === 'win32') {
+  if (os.platform() === 'win32') {
+    console.log("HEJ");
     //Change to the filepath of wkhtmltopdf.exe if you are on windows. 
-    wkhtmltopdf.command = 'c:/program files/wkhtmltopdf/bin/wkhtmltopdf.exe';
+    wkhtmltopdf.command = "C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe";
   }
   //As this is sending the address to another program a relative address cannot be used here.
   wkhtmltopdf('http://localhost:3000/info/' + req.params.slug, { printMediaType: true, output: req.params.slug + '.pdf'});
