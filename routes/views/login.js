1f39c4b6d332a6a334288c10252f2e86b50abcd1
Daniel Rapp <danielrappt@gmail.com>
2015-04-01 16:36:36 +0200

    Initial login window

--- routes/views/login.js
+++ routes/views/login.js
@@ -1,9 +1,10 @@
 var keystone = require('keystone');
 var operation = keystone.list('Operation');
+var session = keystone.session;
 
 exports = module.exports = function (req, res) {
   var view = new keystone.View(req, res),
-    locals = res.locals;
+      locals = res.locals;
 
   locals.section = 'login';
 
@@ -12,8 +13,45 @@ exports = module.exports = function (req, res) {
   ];
 
   locals.css = [
-    'site.css'
+    'site.css',
+    'login.css'
   ];
 
-  view.render('login');
+  var renderView = function() {
+    view.render('login');
+  };
+
+  if (req.method === 'GET') {
+    if (!keystone.security.csrf.validate(req)) {
+      req.flash('error', 'There was an error with your request, please try again.');
+      return renderView();
+    }
+
+    if (!req.body.email || !req.body.password) {
+      req.flash('error', 'Please enter your email address and password.');
+      return renderView();
+    }
+    var onSuccess = function(user) {
+      if (req.query.from && req.query.from.match(/^(?!http|\/\/|javascript).+/)) {
+        res.redirect(req.query.from);
+      } else if ('string' === typeof keystone.get('login redirect')) {
+        res.redirect(keystone.get('login redirect'));
+      } else if ('function' === typeof keystone.get('login redirect')) {
+        keystone.get('login redirect')(user, req, res);
+      } else {
+        res.redirect('/');
+      }
+    };
+
+    var onFail = function() {
+      req.flash('error', 'Sorry, that email and password combo are not valid.');
+      renderView();
+    };
+
+    session.signin(req.body, req, res, onSuccess, onFail);
+  }
+  else {
+    renderView();
+  }
+
 };
