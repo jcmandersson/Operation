596ca1db72b0413b6dcbca68e47153a643ef81d9
Jonas Andersson <jcm.andersson@gmail.com>
2015-04-01 11:06:27 +0200

    Fixed row-grouping on "översikt"

--- templates/views/helpers/index.js
+++ templates/views/helpers/index.js
@@ -323,6 +323,21 @@ module.exports = function() {
     }
     return ret;
   };
+  
+  _helpers.groupedEach = function(every, context, options) {
+    var out = "", subcontext = [], i;
+    if (context && context.length > 0) {
+      for (i = 0; i < context.length; i++) {
+        if (i > 0 && i % every === 0) {
+          out += options.fn(subcontext);
+          subcontext = [];
+        }
+        subcontext.push(context[i]);
+      }
+      out += options.fn(subcontext);
+    }
+    return out;
+  };
 	
 	return _helpers;
 };
