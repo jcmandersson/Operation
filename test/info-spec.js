42676fc5e4b2d32c4cfcba6545f61625d5ddb6dc
erima694 <erima694@student.liu.se>
2015-04-01 14:44:40 +0200

    Added test case to info-spec

--- test/info-spec.js
+++ test/info-spec.js
@@ -2,6 +2,10 @@ describe("Info page tabs", function() {
     $('#0').click(function() {
       tabClick(this); 
     });
+ 
+    $('#all').click(function() {
+      tabClick(this); 
+    });
 
   it("can show the tabs content on click", function() {
     $('#content0').hide();   
@@ -13,6 +17,16 @@ describe("Info page tabs", function() {
     $('#content1').show();   
     $('#0').click();
     expect($('#content1')).toBeHidden();
+  });
+
+  it("can show all contents with All information", function() {
+    $('#content0').hide();   
+    $('#content1').hide();   
+    $('#contentchecklist').show();   
+    $('#all').click();
+    expect($('#content0')).toBeVisible();
+    expect($('#content1')).toBeVisible();
+    expect($('#contentchecklist')).toBeHidden();
     $('#nav-test').remove();
   });
 
