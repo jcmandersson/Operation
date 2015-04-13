c9459af96e2827351552e51b6436e76e1471b543
erima694 <erima694@student.liu.se>
2015-04-13 13:27:19 +0200

    Added box for selecting specialty

---
+++ public/js/list.js
@@ -0,0 +1,34 @@
+var initializeSpecialitetSelect = function () {
+  $(".specialitet-select").select2({
+    ajax: {
+      type: 'GET',
+      url: '/api/search/Specialitet/',
+      dataType: 'json',
+      delay: 250,
+      data: function (params) {
+        return {
+          text: params.term, // search term
+          all: 1
+        };
+      },
+      processResults: function (data) {
+        return {
+          results: $.map(data, function (item) {
+            return {
+              text: item.name, id: item._id
+            }
+          })
+        };
+      },
+      cache: true
+    },
+    escapeMarkup: function (markup) {
+      return markup;
+    }, // let our custom formatter work
+    minimumInputLength: 0
+  });
+};
+
+$(document).ready(function () {
+  initializeSpecialitetSelect();
+});
