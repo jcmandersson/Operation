e888c38800acdc62de7271592ebb7df1029dce0e
Jonas Andersson <jcm.andersson@gmail.com>
2015-04-14 14:07:11 +0200

    Dropdown admin-meny

--- routes/views/listUser.js
+++ routes/views/listUser.js
@@ -7,7 +7,7 @@ exports = module.exports = function (req, res) {
 
   // locals.section is used to set the currently selected
   // item in the header navigation.
-  locals.section = 'userList'
+  locals.section = 'listUser'
 
   locals.scripts = [
     'listUser.js'
