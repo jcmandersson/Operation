ef3e6cb3587e05cdbd4ad0c35fbe0136979f03c6
Jonas Andersson <jcm.andersson@gmail.com>
2015-04-14 13:41:57 +0200

    List with users

--- models/User.js
+++ models/User.js
@@ -1,29 +1,48 @@
 var keystone = require('keystone'),
-  	Types = keystone.Field.Types;
+  Types = keystone.Field.Types;
 
 /**
  * User Model
  * ==========
  */
 
-var User = new keystone.List('User');
+var User = new keystone.List('User', {
+  track: true
+});
 
 User.add({
-	name: { label: 'Namn', type: Types.Name, required: true, index: true },
-	email: { label: 'E-post', type: Types.Email, initial: true, required: true, index: true },
-	password: { label: 'Lösenord', type: Types.Password, initial: true, required: true }
+  name: {label: 'Namn', type: Types.Name, required: true, index: true},
+  email: {label: 'E-post', type: Types.Email, initial: true, required: true, index: true},
+  password: {label: 'Lösenord', type: Types.Password, initial: true, required: true}
 }, 'Permissions', {
-	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
+  isAdmin: {type: Boolean, label: 'Can access Keystone', index: true}
 });
 
 // Provide access to Keystone
-User.schema.virtual('canAccessKeystone').get(function() {
-	return this.isAdmin;
+User.schema.virtual('canAccessKeystone').get(function () {
+  return this.isAdmin;
 });
+
+User.schema.virtual('fullname').get(function () {
+  return this.name.first + ' ' + this.name.last;
+});
+
+User.schema.statics.search = function (text, callback) {
+  var search = new RegExp(text, 'ig');
+  return this.model('User').find({
+    $or: [{
+      'name.first': search
+    },{
+      'name.last': search
+    }, {
+      email: search
+    }]
+  });
+};
 
 User.schema.statics.createUser = function (firstname, lastname, email, password, isAdmin, callback) {
   var UserModel = this.model('User');
-  
+
   new UserModel({
     name: {
       first: firstname,
