72e1c9a1519b15c2bca2a3cc9480902fa2697178
jcmandersson <jcm.andersson@gmail.com>
2015-04-21 11:46:24 +0200

    Mergeconflict...

--- public/js/checkEffect.js
+++ public/js/checkEffect.js
@@ -1,9 +1,9 @@
-var changeTableGraphics = function(tablerow, isChecked, preparation){//Changes the color of tablerows when they are checked.
+var changeTableGraphics = function(tablerow, isChecked, preparation) {//Changes the color of tablerows when they are checked.
   if (isChecked) {
     tablerow.css('background-color', 'rgba(76, 169, 34, 0.2)');
   }
   else {
-    if( preparation) {
+    if (preparation) {
       tablerow.css('background-color', '');
     }
     else {
@@ -12,22 +12,24 @@ var changeTableGraphics = function(tablerow, isChecked, preparation){//Changes t
   }
 };
 
-var changeCommentButton = function (comment, commentButton) {
-  if (comment.val() == "-") comment.val("");
+var changeCommentButton = function(comment, commentButton) {
+  if (comment.val() == "-") {
+    comment.val("");
+  }
   if (comment.val() != "") {
     commentButton.css('background-color','#D9534F');
     commentButton.css('border-color','#D9534F');
-    commentButton.hover(function () {
+    commentButton.hover(function() {
       commentButton.css('background-color', '#C9302C');
-    }, function () {
+    }, function() {
       commentButton.css('background-color', '#D9534F');
     });
     
   }
   else {
-    commentButton.hover(function () {
+    commentButton.hover(function() {
       commentButton.css('background-color', '');
-    }, function () {
+    }, function() {
       commentButton.css('background-color', '')
     });
     commentButton.css('border-color','');
