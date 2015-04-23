db7ad2b0ceb6d7cd083024a8b4090c635e92a49d
jcmandersson <jcm.andersson@gmail.com>
2015-04-23 13:51:53 +0200

    Changed som code to follow the code standard

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-  $('.process-content').each(function(i, e) {
+$(document).ready(function () {
+  $('.process-content').each(function (i, e) {
     var $imgs = $(e).find('.process-content-item img');
-    
-    for(var i = 0; i < $imgs.length; ++i) {
+
+    for (var i = 0; i < $imgs.length; ++i) {
       var $img = $imgs.eq(i);
       var $new = $('<a></a>')
         .addClass('jquery-zoom')
         .css('display', 'block')
         .attr('data-featherlight', 'image')
         .attr('href', $img.attr('src'))
-        .attr('target', '_blank')
+        .attr('target', '_blank');
 
       $img.wrap($new);
     }
