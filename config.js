854b5b6531433870bc856713659c8e788d773423
albka735 <albka735@student.liu.se>
2015-04-23 13:41:06 +0200

    Created a config file for backup and pdfCreate

---
+++ config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by abbe on 2015-04-23.
+ */
+
+var config = {};
+
+// This needs to be set to the path of wkhtmltopdf.exe if the application is used on windows.
+config.pathToWkHtml = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe';
+config.pathToMongodump = '';
+config.pathToBackup = '../backup/';
+config.mongodbConnStr = 'mongodb://localhost:27017/operation';
+config.timeBetweenBackups = 24 * 3600000; // One day
+config.timeBetweenPdfUpdate = 3600000; // One hour
+config.pathToPdf = 'pdf-kopior/';
+
+module.exports = config;
