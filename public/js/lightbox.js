39b1ec894f8602d276f53bbb3fb11b9f2d8a1779
jcmandersson <jcm.andersson@gmail.com>
2015-04-24 10:54:00 +0200

    Moved zoom to fullscreen image

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,27 +1,42 @@
-var initializeImages = function () {
-  $('.process-content-item img').load(function (e) {
-    var $img = $(this);
+var initializeImages = function() {
+  $('.process-content-item img')
+    .load(function(e) {
+      var $img = $(this);
 
-    var $new = $('<a></a>')
-      .css({
-        display: 'block',
-        width: $img.width(),
-        height: $img.height()
-      })
-      .addClass('jquery-zoom')
-      .attr('data-featherlight', 'image')
-      .attr('href', $img.attr('src'))
-      .attr('target', '_blank');
+      var $new = $('<a></a>')
+        .attr('data-featherlight', 'image')
+        .attr('href', $img.attr('src'))
+        .attr('target', '_blank');
 
-    $img.wrap($new);
+      $img.wrap($new);
+    })
+    .click(function() {
+      setTimeout(function() {
+        var $featherlight = $('.featherlight-content:not(.event)').addClass('event');
+        if (!$featherlight.length) return;
 
-    $('.jquery-zoom').removeClass('jquery-zoom').zoom({
-      url: $img.attr('src'),
-      magnify: 1.5
+        var $img = $featherlight.find('img');
+
+        var $new = $('<span></span>')
+          .css({
+            display: 'block',
+            width: $img.width(),
+            height: $img.height(),
+            cursor: 'pointer'
+          })
+          .addClass('jquery-zoom')
+
+        $img.wrap($new);
+
+        $('.jquery-zoom').removeClass('jquery-zoom').zoom({
+          url: $img.attr('src'),
+          magnify: 1.5,
+          on: 'grab'
+        });
+      }, 200);
     });
-  });
 };
 
-$(document).ready(function () {
+$(document).ready(function() {
   initializeImages();
 });
