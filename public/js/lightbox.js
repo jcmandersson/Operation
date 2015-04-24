3f48baebc971c217fa162e78d856dcfcf027f48e
jcmandersson <jcm.andersson@gmail.com>
2015-04-24 09:37:37 +0200

    Fixed bug with image zoom

--- public/js/lightbox.js
+++ public/js/lightbox.js
@@ -1,22 +1,27 @@
-$(document).ready(function () {
-  $('.process-content').each(function (i, e) {
-    var $imgs = $(e).find('.process-content-item img');
+var initializeImages = function () {
+  $('.process-content-item img').load(function (e) {
+    var $img = $(this);
 
-    for (var i = 0; i < $imgs.length; ++i) {
-      var $img = $imgs.eq(i);
-      var $new = $('<a></a>')
-        .addClass('jquery-zoom')
-        .css('display', 'block')
-        .attr('data-featherlight', 'image')
-        .attr('href', $img.attr('src'))
-        .attr('target', '_blank');
+    var $new = $('<a></a>')
+      .css({
+        display: 'block',
+        width: $img.width(),
+        height: $img.height()
+      })
+      .addClass('jquery-zoom')
+      .attr('data-featherlight', 'image')
+      .attr('href', $img.attr('src'))
+      .attr('target', '_blank');
 
-      $img.wrap($new);
-    }
-    var $zoom = $('.jquery-zoom');
-    $zoom.zoom({
-      url: $zoom.find('img').attr('src'),
+    $img.wrap($new);
+
+    $('.jquery-zoom').removeClass('jquery-zoom').zoom({
+      url: $img.attr('src'),
       magnify: 1.5
     });
   });
+};
+
+$(document).ready(function () {
+  initializeImages();
 });
