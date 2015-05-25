0524e254e4c96936e62cf2bb846c2a4e6ee44aba
jcmandersson <jcm.andersson@gmail.com>
2015-05-25 15:39:53 +0200

    Moved alla configs to same file

--- config.js
+++ config.js
@@ -8,12 +8,13 @@ config.port = 3000;
 
 // This needs to be set to the path of wkhtmltopdf.exe if the application is used on windows.
 config.pathToWkHtml = 'C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe';
-config.pathToMongodump = '';
-config.pathToBackup = '../backup/';
-config.mongodbConnStr = 'mongodb://localhost:27017/operation';
-config.timeBetweenBackups = 24 * 3600000; // One day in milliseconds
-config.timeBetweenPdfUpdate = 3600000; // One hour in milliseconds
 config.pathToPdf = 'pdf-kopior/';
+config.timeBetweenPdfUpdate = 3600000; // One hour in milliseconds
+
+config.mongodbConnStr = 'mongodb://localhost:27017/operation';
+config.pathToBackup = '../backup/';
+config.timeBetweenBackups = 24 * 3600000; // One day in milliseconds
+
 // Needs to end with /
 config.siteUrl = 'http://localhost:'+config.port+'/';
 
